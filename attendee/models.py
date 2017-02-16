43f6af2b703f8f2bc4f52053d17426aa7a5f25f2
DeimosA <krvolden@gmail.com>
2017-02-16 11:25:07 +0100

    pin should be unique in db

--- attendee/models.py
+++ attendee/models.py
@@ -22,5 +22,4 @@ class Lecture(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
-    pin = models.CharField(max_length=6, default=generate_pin)
-    # TODO pin must be unique in db
+    pin = models.CharField(max_length=6, default=generate_pin, unique=True)

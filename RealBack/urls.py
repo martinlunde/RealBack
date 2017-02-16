316732d7df133ceee86440c67aca81cfb4965a22
DeimosA <krvolden@gmail.com>
2017-02-16 14:19:33 +0100

    fix lecturer url

--- RealBack/urls.py
+++ RealBack/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('attendee.urls')),
-    url(r'^', include('lecturer.urls')),
+    url(r'^lecturer/', include('lecturer.urls')),
 ]

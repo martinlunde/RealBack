fc9fe34c3928cd0d1cc753cf1d00cdb310d0855d
DeimosA <krvolden@gmail.com>
2017-02-16 14:27:23 +0100

    move away from calling it login page

--- lecturer/urls.py
+++ lecturer/urls.py
@@ -1,6 +1,7 @@
+
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.login_page, name='login_page'),
+    url(r'^$', views.lecturer, name='lecturer_page'),
 ]
--- lecturer/views.py
+++ lecturer/views.py
@@ -1,5 +1,11 @@
+
 from django.shortcuts import render
 
 
-def login_page(request):
+def lecturer(request):
+    # if already logged in:
+
+
+    # else serve login page
     return render(request, 'templates/lecturer/login_page.html', {})
+

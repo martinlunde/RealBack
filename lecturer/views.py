a030125afaf69a53ccdd58b253c5a8bc573b819e
sikaro <petter.thrane@gmail.com>
2017-02-22 13:51:58 +0100

    added lecture_list

--- lecturer/views.py
+++ lecturer/views.py
@@ -1,14 +1,15 @@
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .models import Course
+from .models import Course, Lecture
 
 @login_required  # TODO check for group instead
 def front_page(request):
     """ Lecturer main page """
     course_list = Course.objects.filter(user=request.user)
+    lecture_list = Lecture.objects.filter(user=request.user)
     # TODO pass in list of existing lectures for this user
-    return render(request, 'lecturer/front_page.html', {'courses':course_list})
+    return render(request, 'lecturer/front_page.html', {'courses':course_list, 'lectures':lecture_list})
 
 
 # def login(request):

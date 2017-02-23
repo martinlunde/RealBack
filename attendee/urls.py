318085a18d6fded267fccaf349b12fba6736419c
DeimosA <krvolden@gmail.com>
2017-02-23 13:38:34 +0100

    Should now be working

--- attendee/urls.py
+++ attendee/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^joined$', views.joined, name='joined'),  # Temporary view after successfully joining
+    # url(r'^joined$', views.joined, name='joined'),  # Temporary view after successfully joining
     url(r'^about_us/', views.about_us, name='about'),
+    url(r'^lecture/(?P<pin>[A-Z0-9]{6})/$', views.attend_lecture, name='attend_lecture'),
 ]
--- lecturer/models.py
+++ lecturer/models.py
@@ -1,5 +1,8 @@
 
 from django.db import models, IntegrityError, transaction
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 from random import choice
 from RealBack import settings
 
@@ -21,11 +24,16 @@ def _generate_pin():
 
 class Lecture(models.Model):
     """ Lecture model """
+    # class Meta:
+    #     permissions = (
+    #         ('can_lecture', 'Can access and create lectures'),
+    #         ('can_attend', 'Can attend lectures'),
+    #     )
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     pin = models.CharField(max_length=6, default=_generate_pin, unique=True)
-    pin_user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # pin_user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     # TODO delete the related user with delete() override or post_delete signal
     # TODO free old pins that are not used anymore
 
@@ -39,3 +47,11 @@ class Lecture(models.Model):
             except IntegrityError as err:
                 # TODO maybe log?
                 self.pin = _generate_pin()
+
+
+# @receiver(post_save, sender=Lecture)
+# def create_pin_user(sender, instance, created, **kwargs):
+#     if created:
+#         user = get_user_model().objects.create_user(instance.pin, instance.pin + '@realback.com', instance.pin)
+#         instance.pin_user = user
+#         instance.save()

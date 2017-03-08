a6ee6be3cc994b2b538b087e0875a21c24ca05ba
DeimosA <krvolden@gmail.com>
2017-03-08 15:13:27 +0100

    fix keyerror

--- emailauth/forms.py
+++ emailauth/forms.py
@@ -22,14 +22,14 @@ class UserCreationForm(forms.ModelForm):
     )
 
     def clean_password1(self):
-        password1 = self.cleaned_data['password1']
+        password1 = self.cleaned_data.get('password1')
         validate_password(password1)
         return password1
 
     def clean_password2(self):
-        password1 = self.cleaned_data['password1']
-        password2 = self.cleaned_data['password2']
-        if password1 != password2:
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
             raise forms.ValidationError("The two password fields didn't match.")
         return password2
 

8028fcfdcaf8a49e6bd60944975b464e55c887b4
martinlunde <martlun@stud.ntnu.no>
2017-03-09 13:08:30 +0100

    improved test coverage

--- realback_api/tests.py
+++ realback_api/tests.py
@@ -8,8 +8,53 @@ class ModelTestCase(TestCase):
     def test_generate_pin(self):
         """ Test if attendee login pin is generated """
         pin = models._generate_pin()
+        charset = "ABCDEFGHIJKLMNPQRSTUVWXYZ123456789"
+
+        # Test if pin has a length of 6 and is uppercase
         self.assertEqual(len(pin), 6)
-        # TODO test more properties? character set, all uppercase
+        self.assertEqual(pin, pin.upper())
+
+        # Test if pin i generated with the give charset
+        for i in pin:
+            self.assertEqual(i, charset[charset.rfind(i)])
+
+    def test_return_course(self):
+        user = get_user_model().objects.create_user('test_user', 'test@test.com', 'kNouYH8J3KjJH3')
+        course = models.Course(user=user, title="TDT4180")
+
+        # Testing if course returns a proper string
+        self.assertEqual(str(course), course.title)
+
+        # Testing if course returns a proper json/dictionary
+        course_values = course.as_dict()
+        self.assertEqual(course_values['course_title'], course.title)
+        self.assertEqual(course_values['course_id'], course.id)
+
+    def test_return_lecture(self):
+        user = get_user_model().objects.create_user('test_user', 'test@test.com', 'kNouYH8J3KjJH3')
+        course = models.Course(user=user, title="TDT4145")
+        lecture = models.Lecture(course=course, title="Lecture1")
+
+        # Testing if lecture returns a proper string
+        self.assertEqual(str(lecture), lecture.title)
+
+        # Testing if lecture returns a proper json/dictionary
+        lecture_values = lecture.as_dict()
+        self.assertEqual(lecture_values['lecture_pin'], lecture.pin)
+        self.assertEqual(lecture_values['lecture_title'], lecture.title)
+        self.assertEqual(lecture_values['lecture_start_time'], lecture.start_datetime)
+
+    def test_return_question(self):
+        user = get_user_model().objects.create_user('test_user', 'test@test.com', 'kNouYH8J3KjJH3')
+        course = models.Course(user=user, title="TDT4110")
+        lecture = models.Lecture(course=course, title="questionnaire")
+        question = models.Question(lecture=lecture, text="Why is a rose red?")
+
+        # Testing if question returns a proper json/dictionary
+        question_values = question.as_dict()
+        self.assertEqual(question_values['question_text'], question.text)
+        self.assertEqual(question_values['question_votes'], question.votes)
+
 
     def test_lecture_save(self):
         """ Test if Lecture saves correctly to db """

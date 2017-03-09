845c666d86aaf1974253e89ede6ed1b35c45c02c
DeimosA <krvolden@gmail.com>
2017-03-09 12:16:47 +0100

    question form from view to template

--- attendee/views.py
+++ attendee/views.py
@@ -1,11 +1,13 @@
 
 from django.shortcuts import render
+from realback_api import forms
 
 
 def index(request):
     """ Main index page for RealBack Attenders """
     # TODO necessary forms and info
-    return render(request, 'attendee/index.html', {})
+    question_form = forms.QuestionForm()
+    return render(request, 'attendee/index.html', {'question_form': question_form})
 
 
 def about_us(request):

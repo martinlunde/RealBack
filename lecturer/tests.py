cc1f5f3f6495f19914f12549890fc8405ada4ef1
DeimosA <krvolden@gmail.com>
2017-03-15 13:02:13 +0100

    merge test

--- lecturer/tests.py
+++ lecturer/tests.py
@@ -10,7 +10,7 @@ class WebsiteStabilityTestCase(TestCase):
     def test_availability(self):
         self.assertEqual(c.get('/lecturer/').status_code, 302)  # We are getting redirect when not logged in, so 302
 
-    def test_login(self):
+    def test_lecturer_login(self):
         user = auth.get_user_model().objects.create_user(
             username='test_user',
             email='test@user.com',
@@ -18,12 +18,7 @@ class WebsiteStabilityTestCase(TestCase):
         )
         c.force_login(user=user)
         logged_in_user = auth.get_user(c)
+        # Check that user is authenticated
         self.assertTrue(logged_in_user.is_authenticated)
+        # Check that we are not redirected anymore
         self.assertEqual(c.get('/lecturer/').status_code, 200)
-
-    def test_lecturer_login(self):
-        user = auth.get_user_model().objects.create_user('test_user', 'test@test.com', 'kNouYH8J3KjJH3')
-        user.save()
-
-        # Test if lecturer is logged in upon login-request
-        self.assertEqual(c.post('/login/', {'username': 'test_user', 'password': 'kNouYH8J3KjJH3'}).status_code, 200)

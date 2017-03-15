637198b428ecf7c8e283f0b13e702918ee8a4c64
martinlunde <martlun@stud.ntnu.no>
2017-03-15 10:30:49 +0100

    100% test coverage in attendee views

--- attendee/tests.py
+++ attendee/tests.py
@@ -1,10 +1,13 @@
 
 from django.test import Client, TestCase
-
+from django.urls import reverse
 
 c = Client()
 
 
 class WebsiteStabilityTestCase(TestCase):
-    def test_availability(self):
+    def test_frontpage_availability(self):
         self.assertEqual(c.get('/').status_code, 200)
+
+    def test_about_us_availability(self):
+        self.assertEqual(c.get(reverse('about')).status_code, 200)

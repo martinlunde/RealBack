7fde507a2f01b2eb0ef2a169d4f93f30dae58d15
DeimosA <krvolden@gmail.com>
2017-03-15 12:57:41 +0100

    login test

--- lecturer/tests.py
+++ lecturer/tests.py
@@ -1,6 +1,6 @@
 
 from django.test import Client, TestCase
-
+from django.contrib import auth
 
 c = Client()
 
@@ -8,4 +8,14 @@ c = Client()
 class WebsiteStabilityTestCase(TestCase):
     def test_availability(self):
         self.assertEqual(c.get('/lecturer/').status_code, 302)  # We are getting redirect when not logged in, so 302
-        # TODO maybe check for something more reliable than 302?
+
+    def test_login(self):
+        user = auth.get_user_model().objects.create_user(
+            username='testuser',
+            email='test@user.com',
+            password='testpass1234'
+        )
+        c.force_login(user=user)
+        logged_in_user = auth.get_user(c)
+        self.assertTrue(logged_in_user.is_authenticated)
+        self.assertEqual(c.get('/lecturer/').status_code, 200)

6c3be8d9552718eb32e5e3fbea6c9c1b59e64479
martinlunde <martlun@stud.ntnu.no>
2017-03-15 14:51:33 +0100

    Added more files to .coveragerc, and created tests for user creation form

--- emailauth/tests.py
+++ emailauth/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from emailauth import forms
+from django.contrib import auth
 
-# Create your tests here.
+c = Client()
+
+class FormTests(TestCase):
+
+    def test_creation_form(self):
+        form_data = {'email': 'test@test.com', 'password1': 'test1234', 'password2': 'test1234'}
+        form = forms.UserCreationForm(form_data)
+
+        # Testing if form is valid, and that the fields are working.
+        self.assertTrue(form.is_valid())
+
+    def test_form_save(self):
+        form_data = {'email': 'test@test.com', 'password1': 'test1234', 'password2': 'test1234'}
+        form = forms.UserCreationForm(form_data)
+
+        # Testing if form is valid, and that the fields are working.
+        self.assertTrue(form.is_valid())
+
+        user = form.save()
+        # Testing if save function is returning properly
+        self.assertEqual(str(user), 'test@test.com')
+
+    def test_not_identically_passwords(self):
+        form_data = {'email': 'test@test.com', 'password1': '1234test', 'password2': 'test1234'}
+        form = forms.UserCreationForm(form_data)
+
+        # Testing if form is invalid when passwords are not matching.
+        self.assertFalse(form.is_valid())
+

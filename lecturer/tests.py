bb9142ba1c5d8476a39fa2fc4be98bcef03a602a
martinlunde <martlun@stud.ntnu.no>
2017-03-15 13:28:09 +0100

    Added tests for responses from courses and lectures on the lectureres page

--- lecturer/tests.py
+++ lecturer/tests.py
@@ -1,6 +1,7 @@
 
 from django.test import Client, TestCase
 from django.contrib import auth
+from realback_api import models
 
 
 c = Client()
@@ -22,3 +23,33 @@ class WebsiteStabilityTestCase(TestCase):
         self.assertTrue(logged_in_user.is_authenticated)
         # Check that we are not redirected anymore
         self.assertEqual(c.get('/lecturer/').status_code, 200)
+
+    def test_lecturers_courses(self):
+        user = auth.get_user_model().objects.create_user(
+            username='test_user',
+            email='test@user.com',
+            password='testpass1234'
+        )
+        c.force_login(user=user)
+
+        course = models.Course(user=user, title="TDT4100")
+        course.save()
+
+        # Check if the created course is shown on the lecturers page.
+        self.assertEqual(c.get('/lecturer/TDT4100').status_code, 200)
+
+    def test_courses_lectures(self):
+        user = auth.get_user_model().objects.create_user(
+            username='test_user',
+            email='test@user.com',
+            password='testpass1234'
+        )
+        c.force_login(user=user)
+
+        course = models.Course(user=user, title="TDT4100")
+        course.save()
+        lecture = models.Lecture(course=course, title="test_lecture")
+        lecture.save()
+
+        # Check if the created lecture is listed under its course, and is available to the lecturer.
+        self.assertEqual(c.get('/lecturer/TDT4100/test_lecture').status_code, 200)

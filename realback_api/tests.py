318e1bfbc66f5a6bd9396c5658d2ecdffe61a13b
martinlunde <martlun@stud.ntnu.no>
2017-04-19 13:52:49 +0200

    added tests for LectureStats in api, achieved 54% test coverage

--- realback_api/tests.py
+++ realback_api/tests.py
@@ -131,6 +131,18 @@ class ApiTestCase(TestCase):
         decoded = json.loads(response.content)
         self.assertEqual(decoded['success'], False)
 
+    def testLectureStats(self):
+        user = get_user_model().objects.create_user('test_user', 'test@test.com', 'kNouYH8J3KjJH3')
+        c.login(username='test@test.com', password='kNouYH8J3KjJH3')
+        course = models.Course(user=user, title="TDT4145")
+        course.save()
+        lecture = models.Lecture(course=course, title="Lecture1")
+        lecture.save()
+
+        response = c.get('/courses/' + str(course.id) + '/stats/')
+        decoded = json.loads(response.content)
+        self.assertEqual(decoded['success'], True)
+
 
 class ModelTestCase(TestCase):
     def test_generate_pin(self):
